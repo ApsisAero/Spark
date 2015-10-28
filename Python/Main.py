656f5c9abf9fb880cd1084b8a7363f0df71b8d2e
joshuagruenstein <joshuagruenstein@gmail.com>
2015-10-28 18:46:21 -0400

    testing and zeroing
    confirmed to work

--- Python/Main.py
+++ Python/Main.py
@@ -1,45 +1,45 @@
 import time, serial, thread, sys, datetime
 
 def input_thread(L):
-    raw_input()
+    raw_input("Running. Press enter to end test.")
     L.append(None)
 
 def main():
-	storedVals = []
-	storedTimes = []
-	teensy = serial.Serial('/dev/cu.usbmodem1028921',9600)
+	teensy = serial.Serial("/dev/cu.usbmodem1028921",9600)
+
+	raw_input("Press enter to zero the load cell.")
+
+	subThrust=0.0;
+	for i in range(0,20):
+		subThrust += float(teensy.readline().split(",")[1])
+	subThrust /= 20
 
 	passcode = raw_input('Type passcode to launch: ')
-
 	if passcode != 'spark':
 		return
 
+	firstReadVal = teensy.readline().split(",")
+	subMils = long(firstReadVal[0])
+
 	teensy.write("i".encode('ascii'))
 
-	firstReadVal = teensy.readline().split(",")
-	subMils = float(readVal[0])
+	wFile = open(sys.argv[1],"w")
+	wFile.write("Generated "+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+				+"\nTime (ms),Thrust (lb)\n")
 
 	L = []
 	thread.start_new_thread(input_thread, (L,))
 	while True:
-
 		readVal = teensy.readline().split(",")
 
-		storedTimes.append(float(readVal[0])-subMils)
-		storedVals.append(float(readVal[1]))
+		time = str(long(readVal[0])-subMils)
+		val = str(float(readVal[1])-subThrust)
+
+		wFile.write(time+","+val)
 
 		if L: break
 
 	teensy.write("o".encode('ascii'));
-	wFile = open(sys.argv[0],"w")
-
-	wFile.write("Generated " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
- + "\n")
-
-	for t,v in zip(storedTimes,storedVals):
-		wFile.write(str(storedTimes)+","+str(storedVals)+"\n")
-
-
 	wFile.close()
 
 main()
