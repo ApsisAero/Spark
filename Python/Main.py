9f99e94d0e547a3f7a219f9a5c2d19b5c7051491
joshuagruenstein <joshuagruenstein@gmail.com>
2015-10-28 20:27:51 -0400

    ui and constants

--- Python/Main.py
+++ Python/Main.py
@@ -1,6 +1,8 @@
-import serial, thread, sys, datetime
+import serial, thread, sys, datetime, itertools
 
-ZERO_PRECISION = 20
+ZERO_PRECISION = 100
+SERIAL_BAUD = 9600
+SERIAL_PORT = "/dev/cu.usbmodem1028921"
 
 def input_thread(L):
 	raw_input("Running. Press enter to end test.")
@@ -8,20 +10,30 @@ def input_thread(L):
 
 def main():
 	try:
-		teensy = serial.Serial("/dev/cu.usbmodem1028921",9600)
+		teensy = serial.Serial(SERIAL_PORT,SERIAL_BAUD)
+		print "Successfully connected to Spark board."
 	except OSError:
 		print "Could not find Spark board.  Verify connection."
 		return
 
 	raw_input("Press enter to zero the load cell.")
+	sys.stdout.write("Zeroing ")
 
+	spinner = itertools.cycle(['-', '/', '|', '\\'])
+
+	n = 0
 	subThrust=0.0;
 	for i in range(ZERO_PRECISION):
 		subThrust += float(teensy.readline().split(",")[1])
+		if n%(ZERO_PRECISION/10) == 0:
+			sys.stdout.write(spinner.next())
+			sys.stdout.flush()
+			sys.stdout.write('\b')
 	subThrust /= ZERO_PRECISION
 
-	passcode = raw_input('Type passcode to launch: ')
+	passcode = raw_input('complete. Type passcode to launch: ')
 	if passcode != 'spark':
+		print "Passcode incorrect.  Exiting."
 		return
 
 	firstReadVal = teensy.readline().split(",")
@@ -40,7 +52,7 @@ def main():
 
 		time = str(long(readVal[0])-subMils)
 		val = str(float(readVal[1])-subThrust)
-		
+
 		wFile.write(time+","+val+"\n")
 
 		if L: break
